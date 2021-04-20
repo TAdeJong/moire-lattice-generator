570c40a76f68c78a29a0001f13a22d075fd01df7
T.A. de Jong <tadj42@gmail.com>
2021-04-20 16:11:46 +0200

    Finish numpydoc style docstrings

--- latticegen/singularities.py
+++ latticegen/singularities.py
@@ -12,6 +12,108 @@ from latticegen.transformations import (
 from latticegen.latticegeneration import hexlattice_gen
 
 
+def hexlattice_gen_singularity(r_k, theta, order, size=250,
+                               position=[0, 0], shift=np.array([0, 0]),
+                               **kwargs):
+    """Generate a hexagonal lattice with a singularity.
+
+    Singularity is shifted `position` from the center.
+    Not yet equivalent to hexlattice_gen_singularity_legacy.
+
+    Parameters
+    ----------
+    r_k : float
+        length of lattice vectors in k-space. Larger `r_k` correspond
+        to smaller real space lattice constants.
+    theta : float
+        Angle of the first lattice vector with respect to positive
+        horizontal.
+    order : int
+        Order upto which to generate higher frequency components
+        by combining lattice vectors
+    size: int, or pair of int, default: 500
+        Size of the resulting lattice in pixels. if int, the
+        returned lattice will be square.
+    kappa : float, default: 1
+        strain/deformation magnitude. 1 corresponds to no strain.
+    position : iterable, default [0, 0]
+        [x, y] position of the singularity in pixels with
+        respect to the center.
+    shift : iterable or array, optional
+        shift of the lattice. Either a pair (x,y) global shift,
+        or an (2xNxM) array where (NxM) corresponds to `size`.
+    **kwargs : dict
+        Keyword arguments to be passed to `hexlattice_gen`
+
+    Returns
+    -------
+    lattice : Dask array
+        The generated lattice with singularity
+
+    See Also
+    --------
+    hexlattice_gen, singularity_shift
+    """
+    shift2 = shift + singularity_shift(r_k, theta, size, position)
+    return hexlattice_gen(r_k, theta, order, size, shift=shift2, **kwargs)
+
+
+def singularity_shift(r_k, theta, size=250, position=[0, 0],
+                      alpha=0., symmetry=6):
+    """Generate the shift of an edge dislocation.
+
+    Generate the shift / displacement field
+    for a hexagonal lattice of `size`, with the Burgers vector
+    at angle `alpha` (radians) w.r.t. `theta` (in degrees).
+    Burgers vector has length `np.sin(2*np.pi/symmetry) / r_k`,
+    to create a first order edge dislocation.
+
+    Parameters
+    ----------
+    r_k : float
+        length of lattice vectors in k-space. Larger `r_k` correspond
+        to smaller real space lattice constants.
+    theta : float
+        Angle of the first lattice vector with respect to positive
+        horizontal.
+    order : int
+        Order upto which to generate higher frequency components
+        by combining lattice vectors
+    size: int, or pair of int, default: 500
+        Size of the resulting lattice in pixels. if int, the
+        returned lattice will be square.
+    position : iterable, default: [0, 0]
+        [x, y] position of the singularity in pixels with
+        respect to the center.
+    alpha : float, default 0.0
+        angle in radians of the Burgers vector with respect
+        to the first lattice vector.
+    symmetry : int, default: 6
+        symmetry of the lattice.
+
+    Returns
+    -------
+    shift : ndarray
+        The generated shift corresponding to a singularity,
+        which can be passed to anylattice_gen
+
+    See Also
+    --------
+    anylattice_gen
+    """
+    if not isinstance(size, tuple):
+        size = (size, size)
+    xx = da.arange(-size[0]/2, size[0]/2)[:, None] - position[0]
+    yy = da.arange(-size[1]/2, size[1]/2)[None] - position[1]
+    phi = np.arctan2(yy, xx)
+    phiprime = phi / (2*np.pi)
+    a_0 = 1 / np.sin(2*np.pi / symmetry) / r_k
+    xp = a_0*np.sin(alpha - np.deg2rad(theta)) * phiprime
+    yp = a_0*np.cos(alpha - np.deg2rad(theta)) * phiprime
+    shift = np.array([xp, yp])
+    return shift
+
+
 def hexlattice_gen_singularity_legacy(r_k, theta, order, size=250):
     """Generate a regular hexagonal lattice of `2*size` times `2*size`.
     The lattice is generated from the six 60 degree rotations of `k0`,
@@ -41,43 +143,11 @@ def hexlattice_gen_singularity_legacy(r_k, theta, order, size=250):
           / 2/np.pi/r_k)
     iterated = k_c[:, None, None] * np.exp(np.pi*2j
                * ((xp - 1 * shift[0]) * rks[:, 0, None, None]
-               + (yp - 1 * shift[1]) * rks[:, 1, None, None]))
+                  + (yp - 1 * shift[1]) * rks[:, 1, None, None]))
     iterated = iterated.sum(axis=0)
     # Now add the second shifted sublattice lattice to get a hexagonal lattice
     iterated += (k_c[:, None, None] * np.exp(np.pi * 2j
                  * ((xp + shift[0]) * rks[:, 0, None, None]
-                 + (yp + shift[1]) * rks[:, 1, None, None]))
+                    + (yp + shift[1]) * rks[:, 1, None, None]))
                  ).sum(axis=0)
     return iterated.real
-
-
-def hexlattice_gen_singularity(r_k, theta, order, size=250,
-                               position=[0, 0], shift=np.array([0, 0]),
-                               **kwargs):
-    """Generate a hexagonal lattice with a singularity,
-    shifted `position` from the center.
-    Not yet equivalent to hexlattice_gen_singularity_legacy.
-    """
-    shift2 = shift + singularity_shift(r_k, theta, size, position)
-    return hexlattice_gen(r_k, theta, order, size, shift=shift2, **kwargs)
-
-
-def singularity_shift(r_k, theta, size=250, position=[0, 0],
-                      alpha=0, symmetry=6):
-    """Generate the shift of a edge dislocation at `position`
-    for a hexagonal lattice of `size`, with the Burgers vector
-    at angle `alpha` (radians) w.r.t. `theta` (in degrees).
-    Burgers vector has length `np.sin(2*np.pi/symmetry) / r_k`, to create a first order
-    edge dislocation.
-    """
-    if not isinstance(size, tuple):
-        size = (size, size)
-    xx = da.arange(-size[0]/2, size[0]/2)[:, None] - position[0]
-    yy = da.arange(-size[1]/2, size[1]/2)[None] - position[1]
-    phi = np.arctan2(yy, xx)
-    phiprime = phi / (2*np.pi)
-    a_0 = 1 / np.sin(2*np.pi / symmetry) / r_k
-    xp = a_0*np.sin(alpha - np.deg2rad(theta)) * phiprime
-    yp = a_0*np.cos(alpha - np.deg2rad(theta)) * phiprime
-    shift = np.array([xp, yp])
-    return shift
--- latticegen/transformations.py
+++ latticegen/transformations.py
@@ -3,35 +3,76 @@ import numpy as np
 
 
 def rotation_matrix(angle):
-    """Create an array corresponding to the 2D transformation matrix
+    """Create a rotation matrix
+
+    an array corresponding to the 2D transformation matrix
     of a rotation over `angle`.
+
+    Parameters
+    ----------
+    angle : float
+        rotation angle in radians
+
+    Returns
+    -------
+    ndarray (2x2)
+        2D transformation matrix corresponding
+        to the rotation
     """
     return np.array([[np.cos(angle), -np.sin(angle)],
                      [np.sin(angle), np.cos(angle)]])
 
 
 def scaling_matrix(kappa, dims=2):
-    """Create a numpy array containing the
-    `dims`-dimensional scaling matrix scaling the first dimension by kappa.
+    """Create a scaling matrix
+
+    Creates a numpy array containing the `dims`-dimensional
+    scaling matrix scaling the first dimension by `kappa`.
+
+    Parameters
+    ----------
+    kappa : float
+        scaling factor of first dimension
+    dims : int, default: 2
+        number of dimensions
+    Returns
+    -------
+    ndarray (dims x dims)
+        scaling matrix corresponding
+        to scaling the first dimension by a factor `kappa`
     """
     return np.diag([kappa]+[1]*(dims-1))
 
 
 def apply_transformation_matrix(vecs, matrix):
-    """Apply transformation matrix `matrix` to a list of vectors `vecs`.
+    """Apply transformation matrix to a list of vectors.
+
+    Apply transformation matrix `matrix` to a list of vectors `vecs`.
     `vecs` can either be a list of vectors, or a NxM array, where N is
     the number of M-dimensional vectors.
+
+    Parameters
+    ----------
+    vecs : 2D array or iterable
+        list of vectors to be transformed
+    matrix : 2D array
+        Array corresponding to the transformation matrix
+
+    Returns
+    -------
+    2D array
+        Transformed vectors
     """
     avecs = np.asanyarray(vecs)
     return avecs @ matrix.T
 
 
 def rotate(vecs, angle):
-    "Rotate 2D vectors `vecs` by `angle` around the origin."
+    "Rotate 2D vectors `vecs` by `angle` radians around the origin."
     return apply_transformation_matrix(vecs, rotation_matrix(angle))
 
 
 def wrapToPi(x):
-    """Wrap all values of x to the interval -pi,pi"""
+    """Wrap all values of `x` to the interval [-pi,pi)"""
     r = (x + np.pi) % (2*np.pi) - np.pi
     return r
