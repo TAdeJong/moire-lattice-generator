Add epsilon_to_kappa and fix realspace vs diff scaling
- strain_matrix applied a strain in real space, why diffraction is often
needed.
Sometimes it is needed to convert a strain epsilon into a adapted r_k and
a kappa value, for example to create a lattice from an epsilon value.

tests/test_transformations.py
@@ -29,7 +29,7 @@ def test_identities():
 def test_scaling_matrix(kappa):
     res = scaling_matrix(kappa)
     assert res[0, 0] == kappa
-    assert res.shape == (2,2)
+    assert res.shape == (2, 2)
     res_restore = res.copy()
     res_restore[0, 0] = 1.
     assert np.allclose(res_restore, np.eye(2))