155d97b8c8479677f317ec2c1064d253ea63937d
T.A. de Jong <tadj42@gmail.com>
2021-08-03 23:09:06 +0200

    Version 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Tobias A. de Jong'
 author = 'Tobias A. de Jong'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
